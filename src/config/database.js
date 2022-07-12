f86769bfd28c6c32052d24f970df947db76fb5be
Ronnildo <ronildosp03@gmail.com>
2022-07-11 22:19:19 -0300

    add heroku config

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,17 @@
 module.exports = {
-  development: {
-    dialect: "postgres",
-    host: "localhost",
-    username: "postgres",
-    password: "123456",
-    database: "postgres",
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  },
+  // development: {
+  //   dialect: "postgres",
+  //   host: "localhost",
+  //   username: "postgres",
+  //   password: "123456",
+  //   database: "postgres",
+  //   define: {
+  //     timestamps: true,
+  //     underscored: true,
+  //     underscoredAll: true,
+  //   },
+  // },
 
-  production: {
     use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
@@ -21,5 +20,4 @@ module.exports = {
         rejectUnauthorized: false,
       },
     },
-  },
 };
