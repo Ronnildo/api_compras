e63213b731becef3c7b5e2ca91fb92e28c2c7a1e
Ronnildo <ronildosp03@gmail.com>
2022-07-11 21:38:23 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 33333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log('server port: http://localhost:' + port)
