17aaea626e6477ec1b72905e15dde5ebb09a7714
Ronnildo <ronildosp03@gmail.com>
2022-07-16 13:49:16 -0300

    deployment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const routes = require('./routes');
 
-require('./database');
+require('./database/sequelize');
 
 class App{
     constructor(){
--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 
-const { DATABASE_USERNAME, DATABASE_NAME, DATABASE_HOST, DATABASE_PASSWORD } =
+const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME } =
   process.env;
+
 module.exports = {
-  host: DATABASE_HOST,
-  username: DATABASE_USERNAME,
-  password:DATABASE_PASSWORD,
-  database: DATABASE_NAME,
+  //url: process.env.DATABASE_URL,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
-      rejectUnathorized: false,
+      rejectUnauthorized: false,
     },
   },
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
+    undescoredAll: true,
   },
 };
