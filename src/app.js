cf45e9d4463bfc9834d914d98cf8aabebe07a73c
Ronnildo <ronildosp03@gmail.com>
2022-07-16 13:35:05 -0300

    config deploy

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ class App{
     }
     middlewares(){
         this.server.use(express.json());
+        this.server.use(cors())
     }
 
     routes(){
--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const { DATABASE_USERNAME, DATABASE_NAME, DATABASE_HOST, DATABASE_PASSWORD } =
   process.env;
 module.exports = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: DATABASE_HOST,
+  username: DATABASE_USERNAME,
+  password:DATABASE_PASSWORD,
+  database: DATABASE_NAME,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
