f44e0c985457b9222820f20b8cef22b4530e6b14
Ronnildo <ronildosp03@gmail.com>
2022-07-18 20:36:43 -0300

    Correções

--- src/routes.js
+++ src/routes.js
@@ -1,50 +1,43 @@
-const { Router } = require('express');
-const UserController = require('./app/controllers/UserController');
-const SessionController = require('./app/controllers/SessionController');
-const AuthMiddleware = require('./app/middlewares/auth');
+const { Router } = require("express");
+const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
+const AuthMiddleware = require("./app/middlewares/auth");
 
-const ListaController = require('./app/controllers/ListaController');
-const ItemController = require('./app/controllers/ItemController');
-const CellUserController = require('./app/controllers/CellUserController');
+const ListaController = require("./app/controllers/ListaController");
+const ItemController = require("./app/controllers/ItemController");
+const CellUserController = require("./app/controllers/CellUserController");
 
+const routes = new Router();
 
-const routes =  new Router();
-
-routes.get('/', (req, res) => {
-    return res.json({msg: 'API Compras'});
+routes.get("/", (req, res) => {
+  return res.json({ msg: "API Compras" });
 });
 
+routes.post("/user", UserController.store);
+routes.post("/session", SessionController.store);
 
-routes.post('/user', UserController.store);
-routes.post('/session', SessionController.store);
-
-routes.post('/cell', CellUserController.store);
-routes.get('/cells', CellUserController.index);
+routes.post("/cell", CellUserController.store);
+routes.get("/cells", CellUserController.index);
 
 // Listas
-routes.post('/cell/:cell_id/lista', ListaController.store);
-routes.get('/cell/:cell_id/lista/:lista_id/listas', ListaController.index);
-routes.put('/lista', ListaController.update);
-routes.delete('/lista/:id', ListaController.delete);
+routes.post("/lista", ListaController.store);
+routes.get("/cell/listas", ListaController.index);
+routes.put("/updatelista", ListaController.update);
+routes.delete("/lista/:lista_id", ListaController.delete);
 //routes.post('/lista', ListaController.store);
 
 // Itens
-routes.post('/lista/:lista_id/item', ItemController.store);
-routes.get('/lista/:lista_id/itens', ItemController.index);
-routes.put('/item', ItemController.update);
-routes.delete('/item/:id', ItemController.delete);
+routes.post("/lista/:lista_id/item", ItemController.store);
+routes.get("/lista/:lista_id/itens", ItemController.index);
+routes.put("/updateitem", ItemController.update);
+routes.delete("/item/:item_id", ItemController.delete);
 
-
-routes.use(AuthMiddleware);
+//routes.use(AuthMiddleware);
 // User
-routes.put('/user', UserController.update);
-routes.get('/user', UserController.index)
+routes.put("/user", UserController.update);
+routes.get("/user", UserController.index);
 
 //Test
-
-
-
-
 
 // routes.get("/user", async (req, res) => {
 //     const user = await User.create({
@@ -55,4 +48,4 @@ routes.get('/user', UserController.index)
 //     res.json(user);
 // });
 
-module.exports  =  routes;
+module.exports = routes;
