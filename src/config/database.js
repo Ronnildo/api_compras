9181851b1155d476a856f81359cb89fb505af482
Ronnildo <ronildosp03@gmail.com>
2022-08-01 15:37:26 -0300

    database alterate

--- src/config/database.js
+++ src/config/database.js
@@ -4,22 +4,22 @@ const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME } =
   process.env;
 //console.log(DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME)
 module.exports = {
-  username: "postgres",
+  /*username: "postgres",
   password: "123456",
   database: "postgres",
   host: "localhost",
+  dialect: "postgres",*/
+  url: process.env.DATABASE_URL,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
   dialect: "postgres",
-  // url: process.env.DATABASE_URL,
-  // username: process.env.DATABASE_USERNAME,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_NAME,
-  // host: process.env.DATABASE_HOST,
-  // dialect: "postgres",
-  // dialectOptions: {
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     timestamps: true,
     underscored: true,
